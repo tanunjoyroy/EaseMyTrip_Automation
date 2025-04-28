b2b697ade1fd53964a8c912be0fc82165d2c058b
tanunjoyroy <39924855+tanunjoyroy@users.noreply.github.com>
2025-04-29 01:49:17 +0530

    Update Testbase.java

--- easemytrip_automation/src/main/java/com/easemytrip/qa/base/Testbase.java
+++ easemytrip_automation/src/main/java/com/easemytrip/qa/base/Testbase.java
@@ -50,9 +50,13 @@ public class Testbase {
 			System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 
 			ChromeOptions options = new ChromeOptions();
-			options.addArguments("--headless");
+			options.addArguments("--headless=new");
 			options.addArguments("--no-sandbox");
 			options.addArguments("--disable-dev-shm-usage");
+			options.addArguments("--remote-allow-origins=*");
+			options.addArguments("--disable-gpu");
+			options.addArguments("--window-size=1920,1080");
+			
 			
 			driver = new ChromeDriver(options);
 			
